32e95f165bfe93323761c59fe39bc0be0f5c3979
brahian1x <119307029+brahian1x@users.noreply.github.com>
2022-11-30 10:57:50 -0300

    Update electronica.py

--- electronica.py
+++ electronica.py
@@ -1,38 +1,104 @@
-import tkinter
-from tkinter import ttk
-ve =tkinter.Tk()
-ve.geometry("1050x250")
-ve.resizable(0,0)
-ve.title("Electronica")
-#funciones
-def edit():
-    tabla.insert(e1.get())
 
-#tablas
-tabla=ttk.Treeview(ve,columns=("Articulos","Precios","piesas"))
-tabla.grid(row=0,column=3,columnspan=3)
-tabla.heading("#0",text="id")
-tabla.heading("#1",text="Articulos")
-tabla.heading("#2",text="piesas")
-tabla.heading("#3",text="total")
-tabla.insert("",0,text="1",values=("pc gamer","","200"))
-tabla.insert("",1,text="2",values=("laptop","2000"))
-tabla.place(x=8,y=8)
-#botones
-b4=tkinter.Button(ve,text="Editar")
-b4.grid(row=2,column=3)
-b4.config(width=5,font=("Arial",8,"bold"),fg="white",bg="blue",cursor="hand2",activebackground="#33AFFF")
-b4.place(x=850,y=200)
-b3=tkinter.Button(ve,text="guardar")
-b3.config(width=5,font=("Arial",8,"bold"),fg="white",bg="blue",cursor="hand2",activebackground="#33AFFF")
-b3.place(x=900,y=200)
-b2=tkinter.Button(ve,text="eliminar",command=edit)
-b2.config(width=10,font=("Arial",8,"bold"),fg="white",bg="red",cursor="hand2",activebackground="#BD152E")
-b2.place(x=950,y=200)
-#entrar
-e1=tkinter.Entry(ve,font=("calibri 13"))
-e1.place(x=850,y=100,)
-et1=tkinter.Label(ve,text="agregar articulo:")
-et1.place(x=850,y=77)
+from ast import Import
+from tkinter import Tk, Button, Frame
+from tkinter import CENTER, Entry, Label, Tk, Frame
+import tkinter as tk
+from tkinter.ttk import Combobox, Treeview
+from tkinter import *
 
-ve.mainloop()
+# Crear Ventana
+ventana = Tk()
+ventana.title("Agregar elementos a Treview")
+ventana.geometry("700x300")
+ventana.resizable(0,0)
+
+# Crear Frame
+frame1 = Frame(ventana, bg="#bfdaff")
+frame1.place(x=0, y=0, width=100, height=270)
+
+# Variables Var
+producto = tk.StringVar()
+precio = tk.DoubleVar()
+cantidad = tk.DoubleVar()
+subtotal = tk.DoubleVar()
+total = tk.DoubleVar()
+contador = IntVar()
+
+# Variables lista
+lst_productos = ["pc mesa", "laptop"]
+lst_Precio = [20, 35, 22, 21, 15]
+
+# Variables del TreV
+listaEncabezados = ["Producto", "Precio", "Cantidad", "Subtotal"]
+Columnas = ["#0", "col1", "col2", "col3"]
+
+
+# Funciones de Calculo
+def CargarPrecio(event):
+    producto.set(ventana.cmbProductos.get())
+    precio.set(lst_Precio[ventana.cmbProductos.current()])
+
+
+def Calcular():
+    subtotal.set(cantidad.get() * precio.get())
+    return subtotal.set
+
+
+def Agregar():
+    ventana.tv.insert("", "end", text=producto.get(), values=(precio.get(), cantidad.get(), subtotal.get()))
+
+
+def Total():
+    ""
+
+
+
+# Componentes visuales
+ventana.btnAgregar = Button(frame1, text="Agregar", command=Agregar, bg="azure", fg="DIM GRAY")
+ventana.btnAgregar.place(x=5, y=50, width=80, height=30)
+
+ventana.btnSubtotal = Button(frame1, text="Subtotal", command=Calcular, bg="azure", fg="DIM GRAY")
+ventana.btnSubtotal.place(x=5, y=90, width=80, height=30)
+
+ventana.btnTotal = Button(frame1, text="Total", command=Total, bg="azure", fg="DIM GRAY")
+ventana.btnTotal.place(x=5, y=130, width=80, height=30)
+
+# Frame cajas
+frameCajas = Frame(ventana, bg="#d3dde3")
+frameCajas.place(x=95, y=0, width=195, height=259)
+
+# Declaracion de etiquetas  y cajas
+ventana.lblProducto = Label(frameCajas, text="Producto", bg="bisque2", fg="white",
+                            font=("Courier new", 14, "italic")).place(x=10, y=5)
+ventana.cmbProductos = Combobox(frameCajas, state="readonly")
+ventana.cmbProductos.place(x=10, y=25)
+ventana.cmbProductos["values"] = lst_productos
+ventana.cmbProductos.current(0)
+ventana.cmbProductos.bind("<<ComboboxSelected>>", CargarPrecio)
+
+ventana.lblPrecio = Label(frameCajas, text="Precio", bg="bisque2", fg="white", font=("Courier new", 14, "italic")).place(
+    x=10, y=50)
+ventana.txtPrecio = Entry(frameCajas, textvariable=precio).place(x=10, y=75)
+
+ventana.lblCantidad = Label(frameCajas, text="Cantidad", bg="bisque2", fg="white",
+                            font=("Courier new", 14, "italic")).place(x=10, y=100)
+ventana.txtCantidad = Entry(frameCajas, textvariable=cantidad).place(x=10, y=125)
+
+ventana.lblSubtotal = Label(frameCajas, text="Subtotal", bg="bisque2", fg="white",
+                            font=("Courier new", 14, "italic")).place(x=10, y=150)
+ventana.txtSubtotal = Entry(frameCajas, textvariable=subtotal).place(x=10, y=175)
+
+lblTotal = Label(ventana, text="Total a pagar: ", bg="BISQUE2", fg="white", font=("Courier new", 14, "italic")).place(x=10,
+                                                                                                                   y=250)
+txtTotal = Entry(ventana, textvariable=total, bg="bisque2").place(x=175, y=250, width=50, height=28)
+
+# Tercer Frame Treeview
+frameTv = Frame(ventana, bg="#5D8BF4")
+frameTv.place(x=292, y=0, width=450, height=300)
+ventana.tv = Treeview(frameTv, columns=(Columnas[1], Columnas[2], Columnas[3]))
+for x in range(0, len(Columnas)):
+    ventana.tv.column(Columnas[x], width=80, anchor=CENTER)
+    ventana.tv.heading(Columnas[x], text=listaEncabezados[x], anchor=CENTER)
+ventana.tv.pack()
+
+ventana.mainloop()
